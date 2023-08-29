8b7925408b43a3168eb4502b8105e96c4101d54c
nisyuk <140573790+nisyuk@users.noreply.github.com>
2023-08-29 12:50:22 +0900

    Update calc.js
    条件式を修正

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,11 +1,21 @@
-// 変数numに0～4までのランダムな整数を代入する
-let num = Math.floor(Math.random() * 15);
+// 変数numに0～100までのランダムな整数を代入する
+let num = Math.floor(Math.random() * 100);
 
 // num確認用
 console.log(num + 'がnumに入っている');
 
+// 変数numが0の時はnumを出力
+if(num === 0){
+  console.log(num);
+}
+
+// 変数numが3と5の倍数のときは「3と5の倍数です」という文字列を出力
+else if((num % 3 === 0) && (num % 5 === 0)){
+  console.log('3と5の倍数です');
+}
+
 // 変数numが3の倍数のときは「3の倍数です」という文字列を出力
-if(num % 3 === 0){
+else if(num % 3 === 0){
    console.log('3の倍数です');
 }
 
@@ -14,15 +24,7 @@ else if(num % 5 === 0){
   console.log('5の倍数です');
 }
 
-// 変数numが5の倍数のときは「5の倍数です」という文字列を出力
-else if(num % 3 === 0 && num % 5 === 0){
-  console.log('3と5の倍数です');
-}
-
 // 変数numが3の倍数と5の倍数以外のときはnumを出力
 else{
   console.log(num);
 }
-
-
-
