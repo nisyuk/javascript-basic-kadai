a27e16990e303dd3a971e0f86a2226a39208511a
nisyuk <140573790+nisyuk@users.noreply.github.com>
2023-08-29 10:16:15 +0900

    Update bmi.js
    定数名を修正

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,4 +1,4 @@
 let userWeight = 68;
 let userHeight = 1.7;
-const mbi = (userWeight / (userHeight * userHeight));
-console.log(mbi);
+const bmi = (userWeight / (userHeight * userHeight));
+console.log(bmi);
